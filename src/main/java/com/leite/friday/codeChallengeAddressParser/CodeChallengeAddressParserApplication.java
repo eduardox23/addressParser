d63c2bc5982bb813ef3032b0aec116b365ab62b2
eduardox23 <edd.leite@gmail.com>
2018-11-28 21:24:39 -0200

    initial commit with restful address service

---
+++ src/main/java/com/leite/friday/codeChallengeAddressParser/CodeChallengeAddressParserApplication.java
@@ -0,0 +1,12 @@
+package com.leite.friday.codeChallengeAddressParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeAddressParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeAddressParserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leite/friday/codeChallengeAddressParser/CodeChallengeAddressParserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leite.friday.codeChallengeAddressParser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeChallengeAddressParserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
